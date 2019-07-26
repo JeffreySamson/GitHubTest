9fa54ac64980dfc70faab1d644c77dcb8099fbe1
JeffreySamson <53348502+JeffreySamson@users.noreply.github.com>
2019-07-26 14:10:24 -0400

    first uploads

---
+++ FirstCode.java
@@ -0,0 +1,19 @@
+
+public class FirstCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			boolean temp = true;
+			if(temp){System.out.println("Hello World");}
+			int x = 5;
+			x = x+4;
+			//should give you an error
+			x = 5.0;
+			x = 5.0;
+		
+	}
+
+}
